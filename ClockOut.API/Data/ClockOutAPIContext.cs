6530b94ef0800d922a1c1affe8709b993ee4aac1
Clorkies <clarkisnoob434@gmail.com>
2026-04-02 23:46:22 +0800

    Scaffolded controller for LogEntry

---
+++ ClockOut.API/Data/ClockOutAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClockOut.API.Models;
+
+namespace ClockOut.API.Data
+{
+    public class ClockOutAPIContext : DbContext
+    {
+        public ClockOutAPIContext (DbContextOptions<ClockOutAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ClockOut.API.Models.LogEntry> LogEntry { get; set; } = default!;
+    }
+}
